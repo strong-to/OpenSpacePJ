b6f52d6b0d9d52e2e3be3d4077bace4bfaaabe29
strongTu <strongtu@strongTus-MacBook-Pro.local>
2024-04-29 22:44:19 +0800

    init

--- nft-market-main/contracts/script/Counter.s.sol
+++ nft-market-main/contracts/script/Counter.s.sol
@@ -5,14 +5,15 @@ import {Script, console} from "forge-std/Script.sol";
 
 import {Counter} from "../src/Counter.sol";
 
-contract CounterScript is Script {
+contract CounterScript is Script {qqDD
     function setUp() public {}
 
-    function run() public {
+    function run() public {qww
         vm.broadcast();
 
         Counter c = new Counter();
 
         console.log("Counter address:", address(c));
     }
-}
+}ttwwww+
+DDeerrSS
